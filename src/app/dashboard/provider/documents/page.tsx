0c4a4f665624439c5ffb95e6a704c13f6e48ee3b
Swagat Chand <swagatsaswatchand@gmail.com>
2025-04-24 14:52:47 +0530

    added documents upload and onboarding screens

---
+++ src/app/dashboard/provider/documents/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import DocumentUploadForm from '@/components/provider/DocumentUploadForm';
+
+export default function DocumentUploadPage() {
+  return <DocumentUploadForm />;
+}
---
+++ src/app/dashboard/provider/onboarding/page.tsx
@@ -0,0 +1,8 @@
+ 'use client';
+
+import React from 'react';
+import ProviderOnboardingForm from '@/components/provider/ProviderOnboardingForm';
+
+export default function ProviderOnboardingPage() {
+  return <ProviderOnboardingForm />;
+}
